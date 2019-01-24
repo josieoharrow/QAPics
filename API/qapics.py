16d7233dddd85e00bb00f866e3d403e2f9a5e6ef
Josie O'Harrow <josieoharrow@hotmail.com>
2019-01-23 23:15:26 -0800

    pass in single test case name as arg

--- API/qapics.py
+++ API/qapics.py
@@ -4,11 +4,16 @@ import sys
 sys.path.append('../Analysis/')
 import analyze
 
-baseline_image = Image.open(sys.argv[1], 'r')
-compare_image = Image.open(sys.argv[2], 'r')
+baseline_image_path = "../Baselines/" + sys.argv[1] + ".png"
+compare_image_path = "../" + sys.argv[1] + ".png"
+#Could store screenshot in output and adjust it with filters later.
 
-if sys.argv[3] != None:
-	mask_image = Image.open(sys.argv[3], 'r')
+baseline_image = Image.open(baseline_image_path, 'r')
+compare_image = Image.open(compare_image_path, 'r')
+mask_image = None
+
+if len(sys.argv) > 2:
+	mask_image = Image.open(sys.argv[2], 'r')
 
 def get_differences(baseline_image, compare_image, mask_image):
 	print(analyze.diffs(baseline_image, compare_image, mask_image))
