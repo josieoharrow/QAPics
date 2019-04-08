0d8d88b093df2e279f328334f9051ab15144d161
Josie O'Harrow <josieoharrow@hotmail.com>
2019-04-08 10:43:43 -0700

    Development (#11)
    * cleanup head repo (#10)
    
    * begin work on scan regions

--- API/qapics.py
+++ API/qapics.py
@@ -6,11 +6,12 @@ import analyze
 
 baseline_image_path = "../Baselines/" + sys.argv[1] + ".png"
 compare_image_path = "../" + sys.argv[1] + ".png"
+mask_image =  "../Masks/" + sys.argv[1] + ".png" 
 #Could store screenshot in output and adjust it with filters later.
 
 baseline_image = Image.open(baseline_image_path, 'r')
 compare_image = Image.open(compare_image_path, 'r')
-mask_image = None
+mask_image = Image.open(mask_image, 'r')
 
 if len(sys.argv) > 2:
 	mask_image = Image.open(sys.argv[2], 'r')
